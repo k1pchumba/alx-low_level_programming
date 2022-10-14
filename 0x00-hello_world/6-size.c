0714077d2d69b681c279d4103027cba760e43890
k1pchumba <kipchumbageofry@gmail.com>
2022-10-14 04:12:38 -0700

    AAAV

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,18 @@
  */
 int main(void)
 {
-	int intType;
-	float floatType;
-	double doubleType;
-	char charType;
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float f;
 
-	printf("size of int: %lu bytes \n", sizeof(intType));
-	printf("size of float: %lu bytes\n", sizeof(floatType));
-	printf("size of double: %lu bytes\n", sizeof(doubleType));
-	printf("size of char: %lu bytes \n", sizeof(charType));
-	return (0);
-}
-
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+        printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+        
+return (0);
+} 
+	
