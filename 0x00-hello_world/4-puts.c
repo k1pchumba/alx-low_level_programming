136dda1310a7471884b2a89366a8e4ad316027b8
k1pchumba <kipchumbageofry@gmail.com>
2022-10-14 02:22:30 -0700

    hello. puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
+#!/bin/bash
 #include<stdio.h>
-*comments*
-int main ()
+*/
+*main entry
+*
+ *Return always 0
+*
+* */
+
+int main (void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	getchar();
-		return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
 
