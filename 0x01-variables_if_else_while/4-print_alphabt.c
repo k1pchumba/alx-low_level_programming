d73d77ed40f68fe261a09b44faa0f524077b3abc
k1pchumba <kipchumbageofry@gmail.com>
2022-10-15 01:44:01 -0700

    print aphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,10 @@
-i#include <stdio.h>
+#include <stdio.h>
 
 /**
- * main - program that prints the alphabet in lowercase, followed by a new line
- * You can only use the putchar
- * Return: 0
- */
+* main - program that prints the alphabet in lowercase, followed by a new line
+* You can only use the putchar
+* Return: 0
+*/
 
 int main(void)
 {
@@ -25,3 +25,29 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
