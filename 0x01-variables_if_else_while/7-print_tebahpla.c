e5a57f27fd9281fa227a0e4ca905a2b80e2b52fa
k1pchumba <kipchumbageofry@gmail.com>
2022-10-15 02:15:24 -0700

    numberz

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
 
 /**
- * main -  program that prints all the numbers of base 16 in lowercase
+ * main - program that prints the lowercase alphabet in reverse.
  * You can only use the putchar function
  * Return: 0
  */
 
 int main(void)
 {
-	int n = 48;
-	int l = 'a';
-	while (n <= 57)
+	int n = 122;
+
+	while (n > 96)
 	{
 		putchar(n);
-		n += 1;
-	}
-	while (l <= 'f')
-	{
-		putchar(l);
-		l++;
+		n -= 1;
 	}
 	putchar('\n');
 	return (0);
