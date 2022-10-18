3a9505c4cd602fd99f01b23f2d1b2e90f0304ab3
k1pchumba <kipchumbageofry@gmail.com>
2022-10-18 02:33:05 -0700

    program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int p = "_putchar\n";
+
+	while (*p)
+	{
+		-putchar(*p);
+		P++;
+	}
+	return (0);
+}
+
