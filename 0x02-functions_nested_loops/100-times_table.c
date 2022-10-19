ba73e92776c2d54c033d7db2ea0cdf9c51c96529
k1pchumba <kipchumbageofry@gmail.com>
2022-10-19 02:55:25 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,33 +14,33 @@ void print_times_table(int n)
 	if (n > 0 && n <= 15)
 	{
 		for (a = 0; a <= n; a++)
-		{
+			{
 			_putchar('0');
-		for (b = 1; b <= n; b++)
-		{
-			mt = a * b;
-			_putchar(',');
-			_putchar(' ');
-			if (mt <= 9)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(mt + '0');
+				for (b = 1; b <= n; b++)
+					{
+					mt = a * b;
+					_putchar(',');
+					_putchar(' ');
+					if (mt <= 9)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(mt + '0');
+							}
+					else if (mt <= 99)
+					{
+						_putchar(' ');
+						_putchar((mt / 10) + '0');
+						_putchar((mt % 10) + '0');
+							}
+					else
+						{
+							_putchar(((mt / 100) % 10) + '0');
+							_putchar(((mt / 10) % 10) + '0');
+							_putchar((mt % 10) + '0');
+							}
+					}
+			_putchar('\n');
 			}
-			else if (mt <= 99)
-			{
-				_putchar(' ');
-				_putchar((mt / 10) + '0');
-				_putchar((mt % 10) + '0');
-			}
-			else
-			{
-				_putchar(((mt / 100) % 10) + '0');
-				_putchar(((mt / 10) % 10) + '0');
-				_putchar((mt % 10) + '0');
-			}
-		}
-		_putchar('\n');
 		}
 	}
-}
