ea0ada0504bd5ca6a87081cc2b931478c11599f3
k1pchumba <kipchumbageofry@gmail.com>
2022-11-08 00:34:47 -0800

    created static library

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- *_strpbrk - searches a string for any of a set of bytes
+ * _strpbrk - searches a string for any of a set of bytes
  * @s: string to search
  * @accept: stringcontaining the bytes to look for
  * Return: pointer to the byte in s that matches one of the bytes in accept
